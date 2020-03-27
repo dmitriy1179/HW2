5ced24c4485aaf1c3ee7144acbbfaabb8d3ace01
Dmitriy1179 <chda7911@gmail.com>
2020-03-27 12:21:25 +0200

    HW2

--- index.js
+++ index.js
@@ -1,59 +1,101 @@
 //1
-console.log("My first debug")
+console.log(1 == 2)
+console.log(1 != 2)
+console.log(1 == 1)
+console.log(1 != 1)
+console.log("text" == 5)
+console.log("text" != 5)
+console.log("text" == "Text")
+console.log("text" == "text")
+console.log(true == "true")
+console.log(true == false)
+console.log(false == 0)
+console.log(false === 0)
+console.log("" == false)
+console.log("" !== false)
+console.log(undefined === null)
+console.log(undefined == null)
+console.log(17 === true)
+console.log("17" === true)
+console.log({} === {})
 
 //2
-let petAge = 9
-const petName = "Dog"
-console.log(`${petAge} ${petName}`)
+var sign = prompt("Введите чётное или нечётное число")
+sign %=2
+console.log(Boolean(!sign))
 
 //3
-petAge = 11
-console.log(`${petAge} ${petName}`)
+sign = prompt ("Введите нечётное или чётное число")
+sign %=2
+console.log(Boolean(sign))
 
 //4
-let countOfLivedInUkraine = 9
-const name = "Ivanov"
-const isAdmin = true
-let question = null
-let age
-console.log(typeof countOfLivedInUkraine)
-console.log(typeof name)
-console.log(typeof isAdmin)
-console.log(typeof question)
-console.log(typeof age)
+var numVar = Number(numVar)
+console.log(typeof numVar)
+numVar = prompt("Введите переменную")
+numVar *=10;
+numVar **=2;
+numVar +=100;
+console.log(numVar)
 
 //5
-const student = {
-    name: "Dmitiry",
-    surname: "Cherednichenko",
-    isActive: true
-}
-console.log(typeof student.isActive)
+let a = "100"
+console.log(typeof a)
+a = +a
+console.log(typeof a)
+
+a = String(a)
+console.log(typeof a)
+a = Number(a)
+console.log(typeof a)
+
+a = "100"
+console.log(typeof a)
+a = parseInt(a)
+console.log(typeof a)
 
 //6
-const fruits = ["apple", "orange", "plum", "berry", "mandarin"]
-const counts = [1, 2, 3, 4, 5]
-const students = [
-    {name: "Ivanov",
-    age: 25,
-    faculty: "PMM"},
-    {name: "Petrov",
-    age:23,
-    faculty: "EA"},
-    {name: "Sydorov",
-    age:22,
-    faculty: "KN"},
-    {name: "Andreev",
-    age: 24,
-    faculty: "KISS"},
-    {name: "Nykolaev",
-    age: 19,
-    faculty: "ET"}
-]
+var d = prompt("Введите переменную d")
+var b = prompt("Введите переменную b")
+var c = Number(c)
+var c1 = Number(c1)
+d = Number(d)
+b = Number(b)
+c = (d + b)*(d - b)
+c1 = d**2 - b**2
+console.log(`разность квадратов: ${c===c1}`)
+
 
 //*1
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(`${numbers[0]} ${numbers[9]}`)
+var number1 = prompt("Введите первое число")
+number1 = Number(number1)
+number1 +=10
+var number2 = prompt("Введите второе число")
+number2 = Number(number2)
+number2 +=10
+var number3 = prompt("Введите третье число")
+number3 = Number(number3)
+number3 +=10
+var number4 = Number(number4)
+number4 = number1 + number2 + number3
+window.alert(`Сумма чисел: ${number4}`)
 
 //*2
-console.log(`студент: ${student.name} ${student.surname}, сейчас активен: ${student.isActive}`)
+var side1 = prompt("Введите сторону треугольника A")
+var side2 = prompt("Введите сторону треугольника B")
+var side3 = prompt("Введите сторону треугольника C")
+side1 = Number(side1)
+side2 = Number(side2)
+side3 = Number(side3)
+console.log(`треугольник существует: ${(side1 + side2 > side3) && (side2 + side3 > side1) && (side1 + side3 > side2)}`)
+
+//*3
+var f = prompt("Введите переменную f")
+var e = prompt("Введите переменную e")
+var s = Number(s)
+var s1 = Number(s1)
+f = Number(f)
+e = Number(e)
+s = (f + e)**3
+s1 = f**3 + 3*f**2*e + 3*f*e**2 + e**3
+console.log(`разность кубов: ${s===s1}`)
