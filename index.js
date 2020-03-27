a46cbacf5aa9138c8b4ddf5c3ff4874936d87f39
Dmitriy1179 <chda7911@gmail.com>
2020-03-27 17:49:29 +0200

    HW2

--- index.js
+++ index.js
@@ -55,47 +55,37 @@ a = parseInt(a)
 console.log(typeof a)
 
 //6
-var d = prompt("Введите переменную d")
-var b = prompt("Введите переменную b")
+var d = Number(prompt("Введите переменную d"))
+var b = Number(prompt("Введите переменную b"))
 var c = Number(c)
 var c1 = Number(c1)
-d = Number(d)
-b = Number(b)
 c = (d + b)*(d - b)
 c1 = d**2 - b**2
 console.log(`разность квадратов: ${c===c1}`)
 
 
 //*1
-var number1 = prompt("Введите первое число")
-number1 = Number(number1)
+var number1 = Number(prompt("Введите первое число"))
 number1 +=10
-var number2 = prompt("Введите второе число")
-number2 = Number(number2)
+var number2 = Number(prompt("Введите второе число"))
 number2 +=10
-var number3 = prompt("Введите третье число")
-number3 = Number(number3)
+var number3 = Number(prompt("Введите третье число"))
 number3 +=10
 var number4 = Number(number4)
 number4 = number1 + number2 + number3
 window.alert(`Сумма чисел: ${number4}`)
 
 //*2
-var side1 = prompt("Введите сторону треугольника A")
-var side2 = prompt("Введите сторону треугольника B")
-var side3 = prompt("Введите сторону треугольника C")
-side1 = Number(side1)
-side2 = Number(side2)
-side3 = Number(side3)
+var side1 = Number(prompt("Введите сторону треугольника A"))
+var side2 = Number(prompt("Введите сторону треугольника B"))
+var side3 = Number(prompt("Введите сторону треугольника C"))
 console.log(`треугольник существует: ${(side1 + side2 > side3) && (side2 + side3 > side1) && (side1 + side3 > side2)}`)
 
 //*3
-var f = prompt("Введите переменную f")
-var e = prompt("Введите переменную e")
+var f = Number(prompt("Введите переменную f"))
+var e = Number(prompt("Введите переменную e"))
 var s = Number(s)
 var s1 = Number(s1)
-f = Number(f)
-e = Number(e)
 s = (f + e)**3
 s1 = f**3 + 3*f**2*e + 3*f*e**2 + e**3
 console.log(`разность кубов: ${s===s1}`)
